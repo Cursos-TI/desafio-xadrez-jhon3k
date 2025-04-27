3239c5e04070a05843ae2d7f8b000fdee80e5291
Jhon Arlison da Silva Feitosa <116395900+jhon3k@users.noreply.github.com>
2025-04-27 22:53:35 +0000

    Commit: level adventurous

--- xadrez.c
+++ xadrez.c
@@ -2,9 +2,10 @@
 
 int main() {
 
-    int i, opcao, lado;
+    int i, opcao, lado, movimentoCompleto = 1;
+
     // Entrada para o usuario passa a escolha da peça
-    printf("Escolha uma opção: \n1. Torre\n2. Bispo\n3. Rainha\n");
+    printf("Escolha uma opção: \n1. Torre\n2. Bispo\n3. Rainha\n4. Cavalo\n");
     
     printf("Escolha: ");
     scanf("%d", &opcao);  
@@ -13,7 +14,7 @@ int main() {
     switch (opcao)
     {
     case 1:
-        //Tratando a escolha da direção da peça
+        //Tratando a escolha da direção da peça Torre
         printf("Você escolheu Torre\nEscolha o lado\n1. Direita\n2. Esquerda\nEscolha: ");
         scanf("%d", &lado);
 
@@ -40,7 +41,7 @@ int main() {
     case 2:
         //Resetando o valor de i
         i = 0;
-        //Tratando a escolha da direção da peça
+        //Tratando a escolha da direção da peça Bispo
         printf("Você escolheu Bispo\nEscolha a diagonal\n1. Diagonal Direita\n2. Diagonal Esquerda\nEscolha: ");
         scanf("%d", &lado);
 
@@ -68,6 +69,7 @@ int main() {
     case 3:
         //Resetando o valor de i
         i = 0;
+        //Tratando a escolha da direção da peça Rainha
         printf("Você escolheu Rainha\nEscolha a direção\n1. cima\n2. baixo\n3. Direita\n4. Esquerda\nEscolha: ");
         scanf("%d", &lado);
         switch (lado)
@@ -102,6 +104,57 @@ int main() {
             break;
         default:
             printf("Opção inválida");
+            break;
+        }
+        break;
+    case 4:
+        //Tratando a escolha da direção da peça Cavalo
+        printf("Você escolheu o Cavalo\nEscolha a direção\n1. Cima Direita\n2. Cima Esquerda\n3. Baixo Direita\n4. Baixo Esquerda\nEscolhar: ");
+        scanf("%d", &lado);
+        switch (lado)
+        {
+        case 1:
+            while (movimentoCompleto--)
+            {
+                for ( i = 0; i < 2; i++)
+                {
+                    printf("Cima\n");
+                }
+                printf("Direita\n");
+            }
+            break;
+        case 2:
+            while (movimentoCompleto--)
+            {
+                for ( i = 0; i < 2; i++)
+                {
+                    printf("Cima\n");
+                }
+                printf("Esquerda\n");
+            }
+            break;
+        case 3:
+            while (movimentoCompleto--)
+            {
+                for ( i = 0; i < 2; i++)
+                {
+                    printf("Baixo\n");
+                }
+                printf("Direita\n");
+            }
+            break;
+        case 4:
+            while (movimentoCompleto--)
+            {
+                for ( i = 0; i < 2; i++)
+                {
+                    printf("Baixo\n");
+                }
+                printf("Esquerda\n");
+            }
+            break;
+        default:
+            printf("Opção inválida\n");
             break;
         }
         break;
